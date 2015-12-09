8b98bb8100d71e7e0e1bef1564bc87e5c455b9fa
zhujie <kawasumi37@163.com>
2015-12-10 02:49:53 +0800

    初始化工程

---
+++ First/src/main/java/cc/openphome/HelloServlet.java
@@ -0,0 +1,33 @@
+package cc.openphome;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by kawasumi on 15/12/10.
+ */
+@WebServlet("/hello.view")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        String name = req.getParameter("name");
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1> Hello! " + name + " !</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.close();
+    }
+}
