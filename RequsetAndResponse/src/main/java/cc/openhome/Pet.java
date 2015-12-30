457e9251aa92194411b64e3caf4855d3d19c0873
zhujie <zhujie@51zhangdan.com.cn>
2015-12-30 19:59:40 +0800

    HttpServletResponse.getWrite()

---
+++ RequsetAndResponse/src/main/java/cc/openhome/Pet.java
@@ -0,0 +1,39 @@
+package cc.openhome;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by zhujie on 15/12/30.
+ */
+@WebServlet("/pet")
+public class Pet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>感谢填写</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("联系人:<a href='mailto:" + req.getParameter("email") + "'>" + req.getParameter("user") + "</a>");
+        out.println("<br>喜爱的宠物类型");
+        out.println("<ul>");
+        for (String type : req.getParameterValues("type")) {
+            out.println("<li>" + type + "</li>");
+        }
+        out.println("</ul>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
